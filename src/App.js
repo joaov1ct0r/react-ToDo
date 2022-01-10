eeaa589b4eb703c484413f0699bd277fec3976ca
Joao Victor <jov9051@outlook.com>
2022-01-10 16:50:34 -0300

    form for new toDo's created with text input and button for sending the toDo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div className="toDo-container"></div>
+            <div className="toDo-container">
+                <h1>To Do List</h1>
+
+                <form>
+                    <input type="text" placeholder="Nova tarefa" />
+
+                    <button type="submit">Adicionar</button>
+                </form>
+            </div>
         </div>
     );
 }

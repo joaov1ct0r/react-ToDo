387e9b93cd32993b0c9c6a104c56872a907d6140
Joao Victor <jov9051@outlook.com>
2022-01-10 17:06:36 -0300

    function handleText created inside app.js file and setting state text as input value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+    const [Text, setText] = useState('');
+
+    function handleText(event) {
+        let text = event.target.value;
+
+        setText(text);
+    }
+
     return (
         <div className="App">
             <div className="toDo-container">
                 <h1>To Do List</h1>
 
                 <form>
-                    <input type="text" placeholder="Nova tarefa" />
+                    <input
+                        onChange={handleText}
+                        type="text"
+                        placeholder="Nova tarefa"
+                    />
 
                     <button type="submit">Adicionar</button>
                 </form>

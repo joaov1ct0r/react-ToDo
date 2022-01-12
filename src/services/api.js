d2a78ba4462eda4d80c7d2f77428cd9664512862
Joao Victor <jov9051@outlook.com>
2022-01-11 21:52:31 -0300

    route /edit/:index created with method put changing toDos[index].title for data

--- src/services/api.js
+++ src/services/api.js
@@ -20,6 +20,16 @@ app.post('/new', bodyParser.json(), (req, res) => {
     res.send('Tarefa adicionada com sucesso');
 });
 
+app.put('/edit/:index', bodyParser.json(), (req, res) => {
+    let { index } = req.params;
+
+    let { title } = req.body;
+
+    toDos[index].title = title;
+
+    res.send('Tarefa alterada com sucesso');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });

60311b3c0e19eb75d2050146f119efb0435a2f87
Joao Victor <jov9051@outlook.com>
2022-12-04 19:59:52 -0300

    feat: prop done adicionado a ToDo

--- src/helpers/ToDo.ts
+++ src/helpers/ToDo.ts
@@ -2,7 +2,7 @@ import IToDo from "../interfaces/IToDo";
 import generateId from "./generateId";
 
 export default class ToDo implements IToDo {
-  public id: string;
+  public readonly id: string;
   public text: string;
   public done: boolean;
 

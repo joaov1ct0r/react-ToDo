ce285b782374f630476e81e80d4427b3c60256f3
Joao Victor <jov9051@outlook.com>
2022-12-07 20:45:09 -0300

    refactor: store criado em app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import List from "./components/List";
 import Form from "./components/Form";
-import { configureStore } from "@reduxjs/toolkit";
-import toDoReducer from "./reducers/ToDoReducer";
 import { Provider } from "react-redux";
+import store from "./store";
 
 export default function App(): JSX.Element {
-  const store = configureStore({
-    reducer: {
-      toDo: toDoReducer,
-    },
-  });
-
   return (
     <div className="App">
       <Provider store={store}>

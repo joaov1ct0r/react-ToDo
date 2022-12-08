6a4058657897facedf6e6fdedfda860df5dcac78
Joao Victor <jov9051@outlook.com>
2022-12-08 20:50:25 -0300

    refactor: tarefa sendo enviada a dispatch

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import ToDo from "../helpers/ToDo";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteToDo, markToDo } from "../store/actions/toDoActions";
+import IToDoState from "../interfaces/IToDoState";
 
 export default function List(): JSX.Element {
+  const toDos: ToDo[] = useSelector((state: IToDoState) => {
+    return state.toDo;
+  });
+
+  const dispatch = useDispatch();
+
   return (
     <div className="list-container">
       <ul>
-        {ToDos.map((Tarefa: ToDo) => (
+        {toDos.map((Tarefa: ToDo) => (
           <li id={Tarefa.id} key={Tarefa.id}>
             {" "}
             {Tarefa.text}
-            <button onClick={() => deleteToDo(Tarefa)}>Deletar</button>{" "}
-            <button onClick={() => markToDo(Tarefa)}>Finalizar</button>
+            <button onClick={() => dispatch(deleteToDo(Tarefa))}>
+              Deletar
+            </button>{" "}
+            <button onClick={() => dispatch(markToDo(Tarefa))}>
+              Finalizar
+            </button>
           </li>
         ))}
       </ul>

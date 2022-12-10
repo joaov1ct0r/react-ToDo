748c212d14ccffa2f65f2db928457ebf64a0f2ac
Joao Victor <jov9051@outlook.com>
2022-12-09 21:37:49 -0300

    refactor: store removido de app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import List from "./components/List";
 import Form from "./components/Form";
-import { Provider } from "react-redux";
-import store from "./store";
 
 export default function App(): JSX.Element {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Form></Form>
-        <List></List>
-      </Provider>
+      <Form></Form>
+      <List></List>
     </div>
   );
 }

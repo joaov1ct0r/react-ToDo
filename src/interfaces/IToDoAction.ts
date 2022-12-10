40586e7bd7e30fbabcb20228558ded2733ef8871
Joao Victor <jov9051@outlook.com>
2022-12-09 21:39:38 -0300

    build: favicon removido

--- src/interfaces/IToDoAction.ts
+++
@@ -1,7 +0,0 @@
-import { AnyAction } from "redux";
-import IToDo from "./IToDo";
-
-export default interface IToDoAction extends AnyAction {
-  type: string;
-  payload: IToDo;
-}
--- src/interfaces/IToDoState.ts
+++
@@ -1,5 +0,0 @@
-import IToDo from "./IToDo";
-
-export default interface IToDoState {
-  toDo: IToDo[];
-}

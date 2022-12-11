e37885a9effd0d4ccf45c48b235e01ab1668b198
Joao Victor <jov9051@outlook.com>
2022-12-11 17:51:44 -0300

    feat: tipagem AppDispatch adicionado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
 import { createToDo } from "../store/reducers/toDoSlicer";
 
 export default function Form(): JSX.Element {
   const [Text, setText] = useState<string | null>(null);
 
-  const dispatch = useDispatch();
+  const dispatch: AppDispatch = useDispatch();
 
   function handleText(event: React.ChangeEvent<HTMLInputElement>): void {
     const text: string = event.target.value;

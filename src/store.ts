de83ebf307ca90f978a0d1d4b8a3b8fba402367c
Joao Victor <jov9051@outlook.com>
2022-12-10 21:22:43 -0300

    feat: tipagem ReduxState adicionado

--- src/store.ts
+++ src/store.ts
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import toDoSlicer from "./store/reducers/toDoSlicer";
 
 const store = configureStore({
-  reducer: {
-    toDo: toDoSlicer,
-  },
+  reducer: toDoSlicer,
 });
+
+export type ReduxState = ReturnType<typeof store.getState>;
 
 export default store;

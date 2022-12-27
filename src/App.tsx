7b97758e999de657d7a4c4669a1e53eefbf6cd51
Joao Victor <jov9051@outlook.com>
2022-12-27 20:46:38 -0300

    feat: css in js adicionado a App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,20 @@ import Form from "./components/Form";
 
 export default function App(): JSX.Element {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        display: "block",
+        margin: "auto",
+        marginTop: "5%",
+        maxWidth: "400px",
+        height: "500px",
+        textAlign: "center",
+        backgroundColor: "slategray",
+        borderRadius: "5px",
+        overflowY: "scroll",
+      }}
+    >
       <Form></Form>
       <List></List>
     </div>
